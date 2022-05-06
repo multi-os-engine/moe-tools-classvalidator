98f8aa204058549156aac2c6047c027a85332df2
berstanio <sebastian_grunwald@web.de>
2022-05-06 09:51:33 -0700

    Improve classvalidator - Only save modified classes (cherry-picked) - Copy all dependencie jars to the validator - Do in-jar replacement
    Improve ResourcePackager
    - Fix error with "Project already evaluated" when running with gradle 7 + xcode

--- src/main/kotlin/org/moe/tools/classvalidator/ClassValidator.kt
+++ src/main/kotlin/org/moe/tools/classvalidator/ClassValidator.kt
@@ -1,62 +1,44 @@
 package org.moe.tools.classvalidator
 
-import org.moe.common.utils.classAndJarInputIterator
+import org.moe.common.utils.classpathIterator
 import org.moe.tools.classvalidator.natj.AddMissingAnnotations
 import org.moe.tools.classvalidator.natj.AddMissingNatJRegister
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassVisitor
 import org.objectweb.asm.ClassWriter
-import java.io.BufferedOutputStream
 import java.io.File
-import java.io.FileOutputStream
-import java.nio.file.Path
-import java.util.zip.ZipEntry
-import java.util.zip.ZipOutputStream
 
 object ClassValidator {
     fun process(
-        inputFiles: Set<File>,
-        outputDir: Path,
-        classpath: Set<File>,
+            inputFiles: Set<File>,
+            classpath: Set<File>,
     ) {
         ContextClassLoaderHolder(
-            ChildFirstClassLoader(classpath.map { it.toURI().toURL() }.toTypedArray())
+                ChildFirstClassLoader(classpath.map { it.toURI().toURL() }.toTypedArray())
         ).use {
-            val classSaver = ClassSaver(outputDir.resolve(OUTPUT_CLASSES))
+            inputFiles.forEach { jar ->
+                jar.classpathIterator({ _, inputStream ->
+                    val classSaver = ClassSaver(jar.absoluteFile.toPath())
+                    val cr = ClassReader(inputStream)
 
-            inputFiles.classAndJarInputIterator { _, inputStream ->
-                val cr = ClassReader(inputStream)
-
-                val chain = mutableListOf<ClassVisitor>()
-                fun ClassVisitor.chain(nextBuilder: (ClassVisitor) -> ClassVisitor): ClassVisitor {
-                    val next = nextBuilder(this)
-                    chain.add(next)
-                    return next
-                }
-
-                val byteCode = processClass(cr) { next ->
-                    next
-                        .chain(::AddMissingAnnotations)
-                        .chain(::AddMissingNatJRegister)
-                }
-
-                // Only save modified class
-                if (chain.any { it is ClassModifier && it.modified }) {
-                    classSaver.save(byteCode)
-                }
-            }
-            // TODO: This is really hacky, since r8 seems to only understand jars, not dirs
-            val inputDirectory = outputDir.resolve(OUTPUT_CLASSES).toFile()
-            val outputZipFile = outputDir.resolve("output.jar").toFile()
-            ZipOutputStream(BufferedOutputStream(FileOutputStream(outputZipFile))).use { zos ->
-                inputDirectory.walkTopDown().forEach { file ->
-                    val zipFileName = file.absolutePath.removePrefix(inputDirectory.absolutePath).removePrefix("/")
-                    val entry = ZipEntry( "$zipFileName${(if (file.isDirectory) "/" else "" )}")
-                    zos.putNextEntry(entry)
-                    if (file.isFile) {
-                        file.inputStream().copyTo(zos)
+                    val chain = mutableListOf<ClassVisitor>()
+                    fun ClassVisitor.chain(nextBuilder: (ClassVisitor) -> ClassVisitor): ClassVisitor {
+                        val next = nextBuilder(this)
+                        chain.add(next)
+                        return next
                     }
-                }
+
+                    val byteCode = processClass(cr) { next ->
+                        next
+                                .chain(::AddMissingAnnotations)
+                                .chain(::AddMissingNatJRegister)
+                    }
+
+                    // Only save modified class
+                    if (chain.any { it is ClassModifier && it.modified }) {
+                        classSaver.save(byteCode)
+                    }
+                }, { it.endsWith(".class") })
             }
         }
     }
@@ -74,6 +56,4 @@ object ClassValidator {
 
         return writer.toByteArray()
     }
-
-    const val OUTPUT_CLASSES = "classes"
 }
